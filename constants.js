05328a22ccb6b6ddd98d36498c74243c1929627a
ilovemcjordan <21410866@qq.com>
2021-11-26 19:07:08 +0800

    add files

---
+++ constants.js
@@ -0,0 +1,4 @@
+const URL = 'https://app-s-access-control.roks-dsp-sandbox-t-d4e4e8860bb85ffdb79dd8087e102a98-0000.jp-tok.containers.appdomain.cloud/accessinfo';
+
+const IDNAME = 'アクセスリポジトリ統計図';
+const IPNAME = 'アクセス日統計図';
